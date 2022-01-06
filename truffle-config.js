1e0e4f0ec778c671b527d495ab8af1d5fabe9fa3
Takwa2702 <u17106067@sharjah.ac.ae>
2022-01-06 14:24:24 +0400

    Update files

--- truffle-config.js
+++ truffle-config.js
@@ -54,7 +54,7 @@ module.exports = {
     local: {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+     network_id: "5777",       // Any network (default: none)
     },
     // Another network with more advanced options...
     // advanced: {
