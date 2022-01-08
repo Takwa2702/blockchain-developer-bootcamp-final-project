7304376837c421be75148ee87341547ba285b65a
Takwa2702 <u17106067@sharjah.ac.ae>
2022-01-08 14:15:42 +0400

    Upadate truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -53,7 +53,7 @@ module.exports = {
     //
     local: {
      host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
      network_id: "5777",       // Any network (default: none)
     },
     // Another network with more advanced options...
